947f82db26b8ad46f514315a9aa128918205247b
kr0101 <mohamed442010177@gmail.com>
2022-01-10 13:39:24 +0300

    js file

--- Pen/script.js
+++ Pen/script.js
@@ -1,2 +1,34 @@
-const a = 11
-console.log(a)
+let mybtn = document.getElementById("btn")
+
+
+
+const print = ()=>{
+    console.log(1)
+}
+
+
+
+
+
+
+
+// document.getElementById("btn")
+// document.getElementById("text").innerHTML= 
+
+// '<h1 class = "new-text">jlfj</h1>'
+
+// const btn = document.getElementsByClassName(".btn-next")
+// const type = document.querySelector(".type");
+// let i = 0;
+
+
+// btn.addEventlistener("click", fn())
+
+//    const textArr =
+//    [' hhh ', 'kljkn'];
+
+// type.innerHTML = textArr[i]
+//  i++
+
+// })
+
